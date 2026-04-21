ca7b3190b714a3fca7e7ccbe659abd9e42838e60
Dovudbek <dovudbek@Dovudbeks-MacBook-Air.local>
2026-04-21 13:33:41 +0500

    add

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.MOV"],
-  resolve: {
-    alias: {
-      "@": "/src",
-      // "@": new URL("./src", import.meta.url).pathname,
+    plugins: [react()],
+    assetsInclude: ["**/*.MOV"],
+    resolve: {
+        alias: {
+            "@": "/src",
+            // "@": new URL("./src", import.meta.url).pathname,
+        },
     },
-  },
-})
+});
